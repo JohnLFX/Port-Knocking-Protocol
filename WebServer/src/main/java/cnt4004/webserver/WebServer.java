a3bf2af860580d00a7133c642c855d69ffb695df
JohnLFX <32040938+JohnLFX@users.noreply.github.com>
2018-10-26 16:47:12 -0400

    Add web server code

--- WebServer/src/main/java/cnt4004/webserver/WebServer.java
+++ WebServer/src/main/java/cnt4004/webserver/WebServer.java
@@ -1,10 +1,43 @@
 package cnt4004.webserver;
 
-public class WebServer {
+import fi.iki.elonen.NanoHTTPD;
 
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.util.Map;
 
+public class WebServer extends NanoHTTPD {
 
+    public WebServer(String hostname, int port) {
+        super(hostname, port);
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        WebServer server = new WebServer("localhost", 8080);
+        server.start(NanoHTTPD.SOCKET_READ_TIMEOUT, false);
+
+        System.out.println("Web server is running!");
+
+    }
+
+    public void openTask() throws IOException {
+        this.start();
+    }
+
+    public void closeTask() {
+        this.stop();
+    }
+
+    @Override
+    public Response serve(IHTTPSession session) {
+        String msg = "<html><body><h1>Hello server</h1>\n";
+        Map<String, String> parms = session.getParms();
+        if (parms.get("username") == null) {
+            msg += "<form action='?' method='get'>\n  <p>Your name: <input type='text' name='username'></p>\n" + "</form>\n";
+        } else {
+            msg += "<p>Hello, " + parms.get("username") + "!</p>";
+        }
+        return newFixedLengthResponse(msg + "</body></html>\n");
     }
 
 }
